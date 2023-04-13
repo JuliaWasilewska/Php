04ebc5d009463782d5458add3f149a315ef2344a
JuliaWasilewska <121302186+JuliaWasilewska@users.noreply.github.com>
2023-04-13 17:37:27 +0200

    Update index.php

--- Ćwiczenia 7/index.php
+++ Ćwiczenia 7/index.php
@@ -1,35 +1,38 @@
 <?php
-//ścieżka do folderu z obrazkami
-$dir = 'img/';
-$images = glob($dir . '*{jpg,gif,jpeg}') , GLOB_BRACE //pobranie obrazkow,gifow itp z folderu
-      foreach ($images as $image){
-          echo '<a href="zdjecie.php?file=' . urlencode($image) . '"><img scr=' . $image '"style=height: 100px" /><a>';
+$dir = 'img/'; // ścieżka do folderu z obrazkami
+$images = glob($dir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE); // pobierz obrazki z folderu
+
+foreach ($images as $image) {
+    echo '<a href="zdjecie.php?file=' . urlencode($image) . '"><img src="' . $image . '" style="height: 100px" /></a>';
 }
 ?>
 <?php
 $dir = 'img/thumbnails/';
-$images = glob($dir . '*{jpg,gif,jpeg}') , GLOB_BRACE
+$images = glob($dir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
 
-//stronicowanie
+// stronicowanie
 $limit = 4;
-$page =isset($_GET['page']) ? $_GET['page'] : 1;
-$total_pages = ceil(count($images) / $limit) ;
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$total_pages = ceil(count($images) / $limit);
 $offset = ($page - 1) * $limit;
-$images = array_splice($images,$offset,$limit);
-       foreach ($images as $image) {
-           echo '<a href="zdjecie.php?file=' . urlencode(str_replace('thumbnails/', $image)) . '"><img scr=' . $image '"style=height: 100px" /><a>';
-          }
-       //przyciski stronicowania
-       if ($total_pages> 1) {
+$images = array_slice($images, $offset, $limit);
 
-           echo '<div>';
-           if ($page> 1> ;
-              echo '<a href="index.php?page=' . ($page - 1) . '">Poprzednia strona</a>';
+foreach ($images as $image) {
+    echo '<a href="zdjecie.php?file=' . urlencode(str_replace('thumbnails/', 'full/', $image)) . '"><img src="' . $image . '" style="height: 100px" /></a>';
+}
 
-           if ($page> 1> ;{
-               echo '<a href="index.php?page=' . ($page + 1) . '">Nastepna strona</a>';
-           }
+// przyciski stronicowania
+if ($total_pages > 1) {
+    echo '<div>';
 
-           echo '</div>';
-       }
-       ?>
+    if ($page > 1) {
+        echo '<a href="index.php?page=' . ($page - 1) . '">Poprzednia strona</a> ';
+    }
+
+    if ($page < $total_pages) {
+        echo '<a href="index.php?page=' . ($page + 1) . '">Następna strona</a>';
+    }
+
+    echo '</div>';
+}
+?>
